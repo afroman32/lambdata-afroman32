d7232c6a52f3161fbc6db101ff3e4e8884db3410
Afroman <makoanoble@gmail.com>
2020-05-06 20:02:07 -0600

    Add function for train val test split and function for adding a list to a dataframe as a column

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -1,14 +1,29 @@
-
+# my_mod.py
 
 
 def enlarge(n):
     return n*100
 
+def list_to_col(df, l, name):
+    import pandas as pd
+    new = pd.Series(l)
+    df[name] = new
+    return df
 
+def train_val_test(df):
+    # 60, 20, 20 split
+    train_lim = int(len(df)*.6)
+    val_lim = int(len(df)*.2)+train_lim
+    train = df.iloc[0:train_lim]
+    val = df.iloc[train_lim: val_lim]
+    test = df.iloc[val_lim: len(df)]
 
+    return (train, val, test)
 
-x = 5
-print(enlarge(x))
+if __name__ == "__main__":
+    # only run the code below if running from the command line
+    x = 5
+    print(enlarge(x))
 
-y = int(input("Please chooses a number (eg 5): "))
-print(enlarge(y))
+    y = int(input("Please chooses a number (eg 5): "))
+    print(enlarge(y))
--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,10 +1,19 @@
-
+#my_script.py
 
 from pandas import DataFrame
-
-
-print("HELLO")
+from my_mod import enlarge # this works
+# from my_lambdata.my_mod import enlarge
+from my_mod import list_to_col
 
 df = DataFrame({"a":[1,2,3], "b":[4,5,6],})
-
 print(df)
+
+print()
+
+x = 11
+print(enlarge(x))
+
+print()
+
+l = [7, 8, 9]
+print(list_to_col(df, l, 'c'))
