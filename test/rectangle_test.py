4186a420633925d9abf5d88712a8faabc8858c5f
Afroman <makoanoble@gmail.com>
2020-05-15 14:42:05 -0600

    add rectangle_test module

---
+++ test/rectangle_test.py
@@ -0,0 +1,26 @@
+import unittest
+
+# assume this function exists in my_lambdata/rectangles.py
+def calc_area(l, w):
+    """
+    Params: l and w are both real positive numbers representing length and width of a rectangle
+    """
+    if l<= 0 or w<= 0:
+        raise ValueError
+    return l * w
+
+class TestRectangles(unittest.TestCase):
+
+    def test_area_calculations(self):
+        self.assertEqual(calc_area(5, 3), 15)
+
+        with self.assertRaises(ValueError):
+            calc_area(-5, 3)
+
+
+        with self.assertRaises(ValueError):
+            calc_area(5, -3)
+
+
+if __name__ == '__main__':
+    unittest.main()
